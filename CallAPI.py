9d23d3d8bd8255333a442f4e2d14cfa0c9a1f46b
Elias Fölling <folling@chalmers.se>
2023-03-27 15:56:48 +0200

    TestAPICALL

---
+++ CallAPI.py
@@ -0,0 +1,21 @@
+import requests
+response = requests.get("https://randomuser.me/api/")
+
+def newLines(number):
+    for i in range(number):
+        print('')
+
+
+print(type(response))
+status_code = response.status_code 
+print("Status code is: ", status_code,newLines(10))
+headers = response.headers # represents the headers as a dictionary
+print("Headers is: ", headers, newLines(10))
+text = response.text    # represents the response body as a strings
+print("Body as string is: ", text, newLines(10))
+content = response.content # represents the response body as a binary
+print("Body as binary is: ", content, newLines(10))
+json_format = response.json()  # parses the response body in JSON format
+print("Json format of respose body is: ", json_format, newLines(10))
+request = response.request # represents the request as requests.models.PreparedRequest
+print("Request I sent is: ", request, newLines(10))
